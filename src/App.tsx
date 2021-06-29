ccf7bd24cc729f3f434adf685efe88ea829a8862
TWcamel <j0930495159@gmail.com>
2021-06-30 00:37:48 +0800

    idnticon.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import logo from './resources/icons/logo.svg';
 import "./components/scss/index/App.css";
+import { NavBar } from "./components/NavBar";
+
 
 const App: React.FC = () => {
-	// const [acount, setAccount] = useState<boolean>(false);
+	const [account, setAccount] = useState<string>('3b07384d113edec49eaa6238ad5ff00');
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<NavBar accountHash={account} />
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		</>
 	);
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
-import film from './resources/icons/film.png';
+import React from 'react';
+import film from '../resources/icons/film.png'
 import Identicon from 'identicon.js';
 
-const NavBar: React.FC = () => {
+type GreetingProps = {
+	accountHash: string;
+}
+
+export const NavBar: React.FC<GreetingProps> = ({ accountHash }) => {
 	return (
 		<>
 			<nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 				<a
 					className="navbar-brand col-sm-3 col-md-2 mr-0"
-					href="http://www.dappuniversity.com/bootcamp"
+					href="javascript;"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					<img src={film} width="30" height="30" className="d-inline-block align-top" alt="" />
-					Decentragram
+					dappgram
 				</a>
 				<ul className="navbar-nav px-3">
 					<li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
 						<small className="text-secondary">
 							<small id="account">{'0x0'}</small>
 						</small>
-						{this.props.account
+						{{ accountHash }
 							? <img
+								alt="red dot"
 								className='ml-2'
 								width='30'
 								height='30'
-								src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+								src={`data:image/png;base64,${new Identicon(accountHash, 42).toString()}`}
 							/>
-							: <span></span>
+							: <span>there's no img setup here for now</span>
 						}
 					</li>
 				</ul>
@@ -35,9 +40,3 @@ const NavBar: React.FC = () => {
 		</>
 	);
 }
-
-export default Navbar;
-
-
-
-
